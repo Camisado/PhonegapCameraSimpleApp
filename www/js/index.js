a5a34350a81e688d806d66bea3012e9e8a2e1311
Camisado <YG-92@yandex.ru>
2014-11-28 18:13:59 +0300

    upload photos to server

--- www/js/index.js
+++ www/js/index.js
@@ -10,18 +10,16 @@ function onDeviceReady() {
 
 function onPhotoDataSuccess(imageData) {
     var capturedImage = document.getElementById('capturedImage');
-    var uploadInput = document.getElementById('uploadInput');
     capturedImage.src = imageData;
-    uploadInput.value = imageData;
-    console.log(imageData);
+    console.log("imageData: " + imageData);
+    uploadFile(imageData);
 }
 
 function onPhotoURISuccess(imageURI) {
     var openedImage = document.getElementById('openedImage');
-    var uploadInput = document.getElementById('uploadInput');
     openedImage.src = imageURI;
-    uploadInput.value = imageURI;
-    console.log(imageURI);
+    console.log("imageURI: " + imageURI);
+    uploadFile(imageURI);
 }
 
 function capturePhoto() {
@@ -35,12 +33,40 @@ function capturePhoto() {
 
 function openPhoto() {
     navigator.camera.getPicture(onPhotoURISuccess, onFail, {
-        quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: pictureSource.PHOTOLIBRARY
+        destinationType: window.Camera.DestinationType.FILE_URI,
+        sourceType: window.Camera.PictureSourceType.PHOTOLIBRARY,
+        mediaType: window.Camera.MediaType.PICTURE
     });
 }
 
 function onFail(message) {
     alert('Failed because: ' + message);
+}
+
+function uploadFile (imageURI) {
+    var statusDiv = document.getElementById('status');
+    statusDiv.innerHTML = "";
+    var options = new FileUploadOptions();
+    options.fileKey="file";
+    options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+    console.log("fileName: " + options.fileName);
+    options.mimeType="image/jpeg";
+    options.chunkedMode = false;
+
+    var server = encodeURI('https://touristmapserver.herokuapp.com/upload');
+
+    // Transfer picture to server
+    var ft = new FileTransfer();
+    ft.onprogress = function(progressEvent) {
+        if (progressEvent.lengthComputable) {
+            statusDiv.innerHTML = (progressEvent.loaded / progressEvent.total)*100;
+        }
+    };
+    ft.upload(imageURI, server, function(r) {
+        console.log(JSON.stringify(r));
+        statusDiv.innerHTML = "Upload successful: "+r.bytesSent+" bytes uploaded.";
+    }, function(error) {
+        console.log(JSON.stringify(error));
+        statusDiv.innerHTML = "Upload failed: Code = "+error.code;
+    }, options, true);
 }
